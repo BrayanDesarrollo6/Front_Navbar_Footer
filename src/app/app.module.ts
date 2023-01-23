9978ec9b88d52d2a88417c90739001ae195c76b2
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2023-01-22 22:57:28 -0500

    Navbar y footer completado y funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
